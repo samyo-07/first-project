c598a3a38dd0dc397e417af0f3da0f2d56671384
samyo-07 <samyo.karmakar.07@gmail.com>
2025-08-07 12:44:13 +0530

    testadded

---
+++ ToDolist.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const TodoItem = (props) => <li onClick={props.onClick}>{props.item.text}</li>
+
+const TodoList = ({ items, onListClick, onItemClick }) => {
+    const handleItemClick = (item, event) => {
+        // Write your code here
+        if (item.done) {
+            event.stopPropagation();
+            return;
+        }
+        onItemClick(item, event);
+    };
+
+    return (
+        <ul onClick={onListClick}>
+            {items.map((item, index) =>
+                <TodoItem item={item} key={index} onClick={(event) => handleItemClick(item, event)} />)}
+        </ul>
+    );
+}
+
+const itemsArray = [{ text: 'Buy grocery', done: true },
+{ text: 'Play guitar', done: false },
+{ text: 'Romantic dinner', done: false }
+];
+
+const AppToDoList = (props) => <TodoList
+    items={itemsArray}
+    onListClick={(event) => console.log("List clicked!")}
+    onItemClick={(item, event) => { console.log(item, event) }}
+/>;
+
+export default AppToDoList;
